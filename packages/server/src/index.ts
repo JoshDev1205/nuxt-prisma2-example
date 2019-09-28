c2d85a4ecfc0760d28873c5a76c407a973032bce
Pascal Martineau <pascal@lewebsimple.ca>
2019-09-27 21:58:27 -0400

    GraphQL Yoga

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,24 @@
-const serverPort = process.env.SERVER_PORT || 4000;
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log(`ℹ Listening on http://localhost:${serverPort}`);
+// GraphQL Schema
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: (): string => `Hello World`,
+  },
+};
+
+// GraphQL Server
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+// Start Listening
+const serverOptions = {
+  port: process.env.SERVER_PORT || 4000,
+};
+server.start(serverOptions, () => {
+  console.log(`ℹ Listening on http://localhost:${serverOptions.port}`);
+});
