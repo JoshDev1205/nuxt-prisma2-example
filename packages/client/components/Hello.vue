bd7db518966e226759944062be69fc824c1558cc
Pascal Martineau <pascal@lewebsimple.ca>
2019-10-01 14:30:04 -0400

    Refactor Hello as component

---
+++ packages/client/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+import hello from '@/apollo/queries/hello.gql';
+export default {
+  apollo: {
+    hello: {
+      query: hello,
+    },
+  },
+};
+</script>
--- packages/client/pages/index.vue
+++ packages/client/pages/index.vue
@@ -1,21 +1,16 @@
 <template>
   <div id="app">
     <h1>Nuxt.js / Prisma2</h1>
-    <p>{{ hello }}</p>
+    <Hello />
     <Foo />
   </div>
 </template>
 
 <script>
-import hello from '@/apollo/queries/hello.gql';
 import Foo from '@/components/Foo.vue';
+import Hello from '@/components/Hello.vue';
 
 export default {
-  components: { Foo },
-  apollo: {
-    hello: {
-      query: hello,
-    },
-  },
+  components: { Foo, Hello },
 };
 </script>
