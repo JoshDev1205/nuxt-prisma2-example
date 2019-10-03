8b14487e6ab450b386987b60c3565cc602124c8e
Pascal Martineau <pascal@lewebsimple.ca>
2019-10-03 15:45:58 -0400

    Remove console.log in isAuthenticated Reformat code

--- packages/client/middleware/isAuthenticated.ts
+++ packages/client/middleware/isAuthenticated.ts
@@ -1,6 +1,5 @@
 export default ({ store, route, redirect }) => {
   if (!store.getters['auth/isAuthenticated']) {
-    console.log('not authenticated');
     redirect(`/login?redirect=${route.path || ''}`);
   }
 };
--- packages/client/pages/index.vue
+++ packages/client/pages/index.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <section class="section">
       <div class="container">
-        <h1 class="title">
-          Nuxt.js / Prisma2
-        </h1>
+        <h1 class="title">Nuxt.js / Prisma2</h1>
         <Hello />
         <Foo />
       </div>
@@ -13,10 +11,10 @@
 </template>
 
 <script>
-import Foo from '@/components/Foo.vue';
-import Hello from '@/components/Hello.vue';
+import Foo from "@/components/Foo.vue";
+import Hello from "@/components/Hello.vue";
 
 export default {
-  components: { Foo, Hello },
+  components: { Foo, Hello }
 };
 </script>
