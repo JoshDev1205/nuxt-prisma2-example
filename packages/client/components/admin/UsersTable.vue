46fd82ee6abec394fb08ab93206c941ae9b48a95
Pascal Martineau <pascal@lewebsimple.ca>
2019-10-08 21:02:11 -0400

    Initial UsersTable

---
+++ packages/client/components/admin/UsersTable.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-table :data="users">
+    <template slot-scope="props">
+      <b-table-column field="email" label="Email">{{ props.row.email}}</b-table-column>
+    </template>
+  </b-table>
+</template>
+
+<script>
+import users from "@/apollo/queries/users.gql";
+export default {
+  apollo: {
+    users: {
+      query: users
+    }
+  }
+};
+</script>
---
+++ packages/client/pages/users.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <UsersTable />
+    </div>
+  </section>
+</template>
+
+<script>
+import UsersTable from "@/components/admin/UsersTable";
+export default {
+  components: { UsersTable }
+};
+</script>
